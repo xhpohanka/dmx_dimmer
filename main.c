b25c2a6bbec30a7359773c798ab6fb213812b086
Jan Pohanka <xhpohanka@gmail.com>
2012-11-26 21:15:40 +0100

    PWM settings

--- main.c
+++ main.c
@@ -3,49 +3,91 @@
 
 static void delay(uint32_t x)
 {
-    volatile int i;
+	volatile int i;
 
-    for (i = 0; i < x; i++) ;
+	for (i = 0; i < x; i++) ;
+}
+
+static int pwm_out_init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+	uint16_t PrescalerValue = 0;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_PinRemapConfig(GPIO_FullRemap_TIM3, ENABLE);
+
+	PrescalerValue = (uint16_t) (SystemCoreClock / 24e6) - 1;
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = (24e6/36e3) - 1;
+	TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+	/* PWM1 Mode configuration: Channel3 */
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 10;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+	TIM_ARRPreloadConfig(TIM3, ENABLE);
+	TIM_Cmd(TIM3, ENABLE);
+
+	return 0;
 }
 
 int main(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
 
-    /* GPIOD Periph clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	/* GPIOD Periph clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
-    /* Configure PD0 and PD2 in output pushpull mode */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+	/* Configure PD0 and PD2 in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    int i = 0;
-    while (1)
-    {
-        unsigned reg;
+	pwm_out_init();
 
-        if (i++ % 2) {
-            GPIOA->BSRR = 0x0000FFFF;
-            GPIOB->BSRR = 0x0000FFFF;
-            GPIOC->BSRR = 0x0000FFFF;
-            GPIOD->BSRR = 0x0000FFFF;
-        } else {
-            GPIOA->BRR  = 0x0000FFFF;
-            GPIOB->BRR  = 0x0000FFFF;
-            GPIOC->BRR  = 0x0000FFFF;
-            GPIOD->BRR  = 0x0000FFFF;
+	int i = 0;
+	while (1)
+	{
+		unsigned reg;
 
-        }
+		if (i++ % 2) {
+			GPIOA->BSRR = 0x0000FFFF;
+			GPIOB->BSRR = 0x0000FFFF;
+			GPIOC->BSRR = 0x0000FFFF;
+			GPIOD->BSRR = 0x0000FFFF;
+		} else {
+			GPIOA->BRR  = 0x0000FFFF;
+			GPIOB->BRR  = 0x0000FFFF;
+			GPIOC->BRR  = 0x0000FFFF;
+			GPIOD->BRR  = 0x0000FFFF;
 
-        delay(500000);
-    }
+		}
+
+		delay(500000);
+	}
 
 }
